6ffbce52a4ef33fabeb760d3b319c8f21b9a48f0
DEKO <deko@ht-deko.com>
2017-09-19 01:19:32 +0900

    Update microbit_Screen.cpp

--- microbit_Screen.cpp
+++ microbit_Screen.cpp
@@ -2,17 +2,14 @@
   microbit_Screen.cpp - LED Screen library for micro:bit
   (Arduino Core for Nordic Semiconductor nRF5 based boards)
   Copyright (c) 2017 Hideaki Tominaga. All rights reserved.
-
   This library is free software; you can redistribute it and/or
   modify it under the terms of the GNU Lesser General Public
   License as published by the Free Software Foundation; either
   version 2.1 of the License, or (at your option) any later version.
-
   This library is distributed in the hope that it will be useful,
   but WITHOUT ANY WARRANTY; without even the implied warranty of
   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
   Lesser General Public License for more details.
-
   You should have received a copy of the GNU Lesser General Public
   License along with this library; if not, write to the Free Software
   Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
@@ -67,20 +64,22 @@ bool microbit_Screen::point(const uint8_t x, const uint8_t y) {
   return (screenArr[x][y]);
 }
 
-void microbit_Screen::showData(const uint8_t column, const uint8_t colData) {
-  uint8_t data = colData;
-  for (uint8_t y = 0; y < rowCount; y++) {
-    if (data & 1) {
-      LED_POINT position = LED_POS[y][column];
-      digitalWrite(cols[position.x], LOW );
-      digitalWrite(rows[position.y], HIGH);
-      delay(1);
-      digitalWrite(cols[position.x], HIGH);
-      digitalWrite(rows[position.y], LOW );
-    } else {
-      delay(1);
-    }
-    data = data >> 1;
+void microbit_Screen::showData(const uint8_t *DataArray) {
+  for (uint8_t x = 0; x < colCount; x++) {
+    uint8_t data = DataArray[x];
+    for (uint8_t y = 0; y < rowCount; y++) {
+      if (data & 1) {
+        LED_POINT position = LED_POS[y][x];
+        digitalWrite(cols[position.x], LOW );
+        digitalWrite(rows[position.y], HIGH);
+        delay(1);
+        digitalWrite(cols[position.x], HIGH);
+        digitalWrite(rows[position.y], LOW );
+      } else {
+        delay(1);
+      }
+      data = data >> 1;
+    }  
   }  
 }
 
@@ -118,9 +117,7 @@ void microbit_Screen::showString(const String str) {
   do {
     unsigned long tick = millis();
     do {
-      for (uint8_t x = 0; x < colCount; x++) {
-        showData(x, strBuf[x + idx]);
-      }
+      showData(&strBuf[idx]);
     } while ((millis() - tick) < 150); 
     idx++;
   } while ((idx < dStr.length() * colCount + colCount) && !isSingleChar);
@@ -136,9 +133,7 @@ void microbit_Screen::showNumber(const int value) {
 void microbit_Screen::showIcon(const IconNames icon) {
   unsigned long tick = millis();
   do {
-    for (uint8_t x = 0; x < colCount; x++) {
-      showData(x, LED_ICON[(int)icon][x]);
-    }
+    showData(LED_ICON[(int)icon]);
   } while ((millis() - tick) < 750);  
 }
 
@@ -146,9 +141,7 @@ void microbit_Screen::showArrow(const uint8_t direction) {
   uint8_t d = direction % 8;
   unsigned long tick = millis();
   do {
-    for (uint8_t x = 0; x < colCount; x++) {
-      showData(x, LED_ARROW[d][x]);
-    }
+    showData(LED_ARROW[d]);
   } while ((millis() - tick) < 750);  
 }
 
