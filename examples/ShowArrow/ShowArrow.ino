3ee52229e5eceb17d1168bf06260ab9fa0a16f4f
DEKO <deko@ht-deko.com>
2017-09-20 03:30:16 +0900

    Update ShowArrow.ino

--- examples/ShowArrow/ShowArrow.ino
+++ examples/ShowArrow/ShowArrow.ino
@@ -5,6 +5,16 @@ void setup() {
 }
 
 void loop() {
+  // CW
   for (int i = 0; i < 8; i++)
-    SCREEN.showArrow(i);
+    SCREEN.showArrow((ArrowNames)i);
+  // CCW
+  SCREEN.showArrow(ArrowNames::North);
+  SCREEN.showArrow(ArrowNames::NorthWest);
+  SCREEN.showArrow(ArrowNames::West);
+  SCREEN.showArrow(ArrowNames::SouthWest);
+  SCREEN.showArrow(ArrowNames::South);
+  SCREEN.showArrow(ArrowNames::SouthEast);
+  SCREEN.showArrow(ArrowNames::East);
+  SCREEN.showArrow(ArrowNames::NorthEast);
 }
